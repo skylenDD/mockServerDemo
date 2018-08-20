b56d05734535021eda0ba69bd881795563cd3c30
chenhaoming <chenhaoming@3tmall.com>
2018-08-20 18:18:20 +0800

    first commit for environment

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor() {
+    console.log(environment.production); // Logs false for default environment
+  }
+
+  title = 'app';
+  environment = environment.envName;
+}
