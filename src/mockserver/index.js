e6c0268d22e69c0ca0bfca513a6778cc7dce5041
chenhaoming <18771902283@163.com>
2018-11-15 21:11:34 +0800

    repaire the nodemon

--- src/mockserver/index.js
+++ src/mockserver/index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
-const openAuthorityResponse = require('./user/author')
+// const openAuthorityResponse = require('./user/author');
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
@@ -20,11 +20,11 @@ server.use('/system/user', (req, res) => {
   });
 });
 
-server.use('/system/openAuthority', (req, res) => {
-  if (req.method === 'POST') {
-    res.jsonp(openAuthorityResponse);
-  }
-});
+// server.use('/system/openAuthority', (req, res) => {
+//   if (req.method === 'POST') {
+//     res.jsonp(openAuthorityResponse);
+//   }
+// });
 
 server.listen(4600, () => {
   console.log('JSON server in running on http://localhost:4600');
